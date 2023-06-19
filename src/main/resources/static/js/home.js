d59e7d233236ccb5c880ec5936a5918017f5b896
Obata <Obata.Seiya@technopro.com>
2023-06-19 17:37:31 +0900

    共通部品作成

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,4 +1,10 @@
 document.getElementById("input").onkeyup = function() {
     let count = this.value.length;
+    if(count > 20){
+        document.getElementById("count").style.color="red";
+    }
+    else{
+        document.getElementById("count").style.color="";
+    }
     document.getElementById("count").innerHTML = "文字数：" + count;
 }
