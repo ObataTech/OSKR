23205da55561dedbeff8f007ab8916494e83eec8
Obata <Obata.Seiya@technopro.com>
2023-06-20 11:52:46 +0900

    機能実装: Bootstrapをspringbootで使用できるように変更

---
+++ src/main/java/com/example/home/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("home")
+public class HomeController {
+
+	//画面表示
+	@GetMapping
+	public String HomeList() {
+		return "home/home";
+	}
+}
